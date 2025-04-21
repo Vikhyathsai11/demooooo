6e4a3d0baf42d06333ed46eb4598917bbcf1e453
vikhyathsai11 <vikhyathsai11@gmail.com>
2025-04-21 20:56:12 +0530

    dfghjk

--- client/alp/src/App.jsx
+++ client/alp/src/App.jsx
@@ -32,18 +32,7 @@ function handleLogin(event) {
 function App() {
   return (
     <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <h1>hello world</h1>
     </div>
   );
 }
